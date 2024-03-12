f1391f09cda8bf97a1c893518aa2d141cc9a8f98
JandryNexia <118757692+JandryNexia@users.noreply.github.com>
2024-03-12 16:12:02 +0100

    fixing dbs

--- database/migrations/2024_02_28_125857_create_users_table.php
+++ database/migrations/2024_02_28_125857_create_users_table.php
@@ -13,6 +13,11 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('email');
+            $table->text('description');
+            $table->string('bg_image');
+            $table->string('profile_image');
             $table->timestamps();
         });
     }
--- database/migrations/2024_02_28_140128_create_user_items_table.php
+++ database/migrations/2024_02_28_140128_create_user_items_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\User;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -13,6 +14,13 @@ return new class extends Migration
     {
         Schema::create('user_items', function (Blueprint $table) {
             $table->id();
+            $table->string('text_color');
+            $table->string('bg_color');
+            $table->string('bg_image');
+            $table->string('url');
+            $table->string('icon');
+            $table->foreignIdFor(User::class)->constrained()
+            ->cascadeOnDelete();
             $table->timestamps();
         });
     }
