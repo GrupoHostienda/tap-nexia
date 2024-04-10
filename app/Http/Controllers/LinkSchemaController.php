7ee101171b25b1fdd3387674ae046a3046e174fd
joelnexia <joel@nexia.io>
2024-04-10 11:43:03 +0200

    Login api and back office

---
+++ app/Http/Controllers/LinkSchemaController.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Inertia\Inertia;
+use App\Models\LinkType;
+use App\Models\LinkSchema;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Redirect;
+
+class LinkSchemaController extends Controller
+{
+     /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return Inertia::render('LinkSchemas/index', [
+            'linkTypes' => LinkType::all(),
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create(Request $request)
+    {
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        LinkSchema::create($request->all());
+
+        return Redirect::back()->with([
+            'message' => 'Example created successfully',
+        ]);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(LinkSchema $linkType)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(LinkSchema $linkType)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, LinkSchema $linkType)
+    {
+        $element = LinkSchema::find($request->id);
+        $element->update($request->all());
+
+        return Redirect::back()->with([
+            'message' => 'Example updated successfully',
+        ]);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Request $request, $id)
+    {
+        LinkSchema::destroy($id);
+
+        return Redirect::back()->with([
+            'message' => 'Example deleted successfully',
+            'buttons' => LinkSchema::all(),
+        ]);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function deleteSelected(Request $request)
+    {
+
+        $exampleIds = collect($request->items)->pluck('id');
+        LinkSchema::destroy($exampleIds);
+
+        return Redirect::back()->with([
+            'message' => 'Selected link types deleted successfully',
+            'buttons' => LinkSchema::all(),
+        ]);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -3,10 +3,10 @@
 use Inertia\Inertia;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Foundation\Application;
+// Controllers
 use App\Http\Controllers\ExampleController;
 use App\Http\Controllers\ProfileController;
-use App\Http\Controllers\ItemController;
-use App\Http\Controllers\UserSocialItemController;
+use App\Http\Controllers\LinkTypeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -28,28 +28,28 @@ Route::get('/', function () {
     ]);
 });
 
-
-Route::prefix('/items')->middleware('auth')->group(function () {
-    Route::get('/', [ItemController::class, 'index'])->name('items');
-    Route::post('/', [ItemController::class, 'store'])->name('items.store');
-    Route::post('update', [ItemController::class, 'update'])->name('items.update');
-    Route::delete('/deleteSelected', [ItemController::class, 'deleteSelected'])->name('items.deleteSelected');
-    Route::delete('/{example}', [ItemController::class, 'destroy'])->name('items.destroy');
-    // Route::get('/create', [ItemController::class, 'create'])->name('items.create');
-    // Route::get('/{example}', [ItemController::class, 'show'])->name('items.show');
-    // Route::get('/{example}/edit', [ItemController::class, 'edit'])->name('items.edit');
+Route::prefix('/link-types')->middleware('auth')->group(function () {
+    Route::get('/', [LinkTypeController::class, 'index'])->name('linkType');
+    Route::post('/', [LinkTypeController::class, 'store'])->name('linkType.store');
+    Route::post('update', [LinkTypeController::class, 'update'])->name('linkType.update');
+    Route::delete('/deleteSelected', [LinkTypeController::class, 'deleteSelected'])->name('linkType.deleteSelected');
+    Route::delete('/{type}', [LinkTypeController::class, 'destroy'])->name('linkType.destroy');
+    // Route::get('/create', [LinkTypeController::class, 'create'])->name('items.create');
+    // Route::get('/{example}', [LinkTypeController::class, 'show'])->name('items.show');
+    // Route::get('/{example}/edit', [LinkTypeController::class, 'edit'])->name('items.edit');
 });
 
-Route::prefix('/socialItems')->middleware('auth')->group(function () {
-    Route::get('/', [UserSocialItemController::class, 'index'])->name('socialItems');
-    Route::post('/', [UserSocialItemController::class, 'store'])->name('socialItems.store');
-    Route::post('update', [UserSocialItemController::class, 'update'])->name('socialItems.update');
-    Route::delete('/deleteSelected', [UserSocialItemController::class, 'deleteSelected'])->name('socialItems.deleteSelected');
-    Route::delete('/{example}', [UserSocialItemController::class, 'destroy'])->name('socialItems.destroy');
-    // Route::get('/create', [UserSocialItemController::class, 'create'])->name('socialItems.create');
-    // Route::get('/{example}', [UserSocialItemController::class, 'show'])->name('socialItems.show');
-    // Route::get('/{example}/edit', [UserSocialItemController::class, 'edit'])->name('socialItems.edit');
+Route::prefix('/link-schemas')->middleware('auth')->group(function () {
+    Route::get('/', [LinkTypeController::class, 'index'])->name('linkType');
+    Route::post('/', [LinkTypeController::class, 'store'])->name('linkType.store');
+    Route::post('update', [LinkTypeController::class, 'update'])->name('linkType.update');
+    Route::delete('/deleteSelected', [LinkTypeController::class, 'deleteSelected'])->name('linkType.deleteSelected');
+    Route::delete('/{type}', [LinkTypeController::class, 'destroy'])->name('linkType.destroy');
+    // Route::get('/create', [LinkTypeController::class, 'create'])->name('items.create');
+    // Route::get('/{example}', [LinkTypeController::class, 'show'])->name('items.show');
+    // Route::get('/{example}/edit', [LinkTypeController::class, 'edit'])->name('items.edit');
 });
+
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
