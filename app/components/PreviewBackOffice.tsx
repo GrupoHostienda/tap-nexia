e53d7dda94fb89d96fe8d905434a404544c19dbc
Alejandro Veleiro <veleiroalejandro@gmail.com>
2024-04-11 11:59:08 -0400

    preview and scrollbars

---
+++ app/components/PreviewBackOffice.tsx
@@ -0,0 +1,12 @@
+
+function PreviewBackOffice(){
+    return <>
+    <div className="flex self-center bg-black rounded-2xl w-64 h-[28rem] p-3">
+        <div className="w-full h-full bg-white rounded-2xl p-3 grid grid-cols-1 gap-4 justify-items-center overflow-y-scroll hidden-scrollbar">
+<div className="size-16 rounded-full bg-gray-700"></div>
+        </div>
+    </div>
+    </>
+}
+
+export default PreviewBackOffice
--- app/routes/backOffice.tsx
+++ app/routes/backOffice.tsx
@@ -1,7 +1,7 @@
 import BackOfficeMenu from "@/components/BackOfficeMenu";
 import CardBackOffice from "@/components/CardBackOffice";
-import data from "data.json"
-import { isReactElement } from "framer/utils/type-guards.js";
+import PreviewBackOffice from "@/components/PreviewBackOffice";
+import data from "data.json";
 export function meta() {
   return [
     {
@@ -15,26 +15,24 @@ export function meta() {
 }
 
 export default function LayoutBackOffice() {
-
-  const {links} = data
+  const { links } = data;
 
   return (
     <>
-      <div className="absolute top-0 w-full h-[100%] bg-slate-200 left-0 grid grid-cols-[60%_20%] gap-10">
+      <div className="absolute top-0 w-full h-[100%] bg-slate-200 left-0 grid grid-cols-[60%_30%] gap-10">
         <div className="ml-10 w-full h-screen pt-5 px-5 flex flex-col gap-2">
-
-          <BackOfficeMenu/>
-          <div className="flex flex-col gap-4 p-3 overflow-y-scroll h-screen">
-
-          {links.map((link, index) =>{
-            return <CardBackOffice text={link.title} url={link.url} id={index} />
-          })}
-
+          <BackOfficeMenu />
+          <div className="flex flex-col gap-4 p-3 overflow-y-scroll h-screen hidden-scrollbar">
+            {links.map((link, index) => {
+              return (
+                <CardBackOffice text={link.title} url={link.url} id={index} />
+              );
+            })}
           </div>
         </div>
-{/* Preview de elementos */}
-        <div className="w-full h-screen">
-            <span>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Error alias iure repellat, impedit et qui facilis pariatur aut. Unde sunt fugiat ipsum ipsa culpa, iusto aut facilis! Eaque, est omnis.</span>
+        {/* Preview de elementos */}
+        <div className="w-full h-screen flex items-center justify-center">
+          <PreviewBackOffice />
         </div>
       </div>
     </>
