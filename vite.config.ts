9bbe1eeb3e15ddbb66dd2e6623beaf8187b0eded
Pedro Nexia <108832942+GrupoHostienda@users.noreply.github.com>
2024-05-17 16:11:15 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,16 @@
-import { vitePlugin as remix } from "@remix-run/dev";
-import { installGlobals } from "@remix-run/node";
-import { defineConfig } from "vite";
-import tsconfigPaths from "vite-tsconfig-paths";
-
+import { vitePlugin as remix } from '@remix-run/dev';
+import { installGlobals } from '@remix-run/node';
+import { defineConfig } from 'vite';
+import tsconfigPaths from 'vite-tsconfig-paths';
+import { vercelPreset } from '@vercel/remix/vite';
+ 
 installGlobals();
-
+ 
 export default defineConfig({
-  plugins: [remix(), tsconfigPaths()],
-  server: {
-    port: 3000,
-  },
+  plugins: [
+    remix({
+      presets: [vercelPreset()],
+    }),
+    tsconfigPaths(),
+  ],
 });
